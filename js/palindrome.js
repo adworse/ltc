96377c91467e845342952538421ac981a2e19b75
adworse <wlaer@wlaer.com>
2019-03-23 00:38:30 +0300

    palindrome.js no str.replace

--- js/palindrome.js
+++ js/palindrome.js
@@ -1,12 +1,17 @@
-function isPalindrome(str) {
-  function check(string) {
-    let len = string.length
-    if (len < 2) {
-      return true
-    } else if (string[0] === string[len - 1]) {
-      return isPalindrome(string.substr(1, len - 2))
-    }
-    return false
+function isPalindrome(string) {
+  function notLetter(character) {
+    return !!character.match(/[^\p{L}\p{N}]/u)
   }
-  return check(str.replace(/[^\p{L}\p{N}]/gu, '').toLowerCase())
+
+  let len = string.length
+  if(len < 2) {
+    return true
+  } else if(notLetter(string[0])) {
+    return isPalindrome(string.substr(1))
+  } else if(notLetter(string[len - 1])) {
+    return isPalindrome(string.substr(0, len - 1))
+  } else if (string[0].toLowerCase() === string[len - 1].toLowerCase()) {
+    return isPalindrome(string.substr(1, len - 2))
+  }
+  return false
 }
