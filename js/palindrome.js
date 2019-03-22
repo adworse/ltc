0c28241e47c1e2d49363b17bd392ba2198f96cec
adworse <wlaer@wlaer.com>
2019-03-23 01:34:39 +0300

    palindrome.js no str.replace

--- js/palindrome.js
+++ js/palindrome.js
@@ -1,17 +1,13 @@
 function isPalindrome(string) {
   function notLetter(character) {
-    return !!character.match(/[^\p{L}\p{N}]/u)
+    return character.match(/[^\p{L}\p{N}]/u)
   }
 
   let len = string.length
-  if(len < 2) {
-    return true
-  } else if(notLetter(string[0])) {
-    return isPalindrome(string.substr(1))
-  } else if(notLetter(string[len - 1])) {
-    return isPalindrome(string.substr(0, len - 1))
-  } else if (string[0].toLowerCase() === string[len - 1].toLowerCase()) {
+  if(len < 2) return true
+  if(notLetter(string[0])) return isPalindrome(string.substr(1))
+  if(notLetter(string[len - 1])) return isPalindrome(string.substr(0, len - 1))
+  if (string[0].toLowerCase() === string[len - 1].toLowerCase())
     return isPalindrome(string.substr(1, len - 2))
-  }
   return false
 }
