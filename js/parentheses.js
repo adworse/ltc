06a69bf0e9e2f6fa15e0c31cba6b576639fef915
adworse <wlaer@wlaer.com>
2019-03-25 14:06:54 +0300

    Get rid of str.replace

--- js/parentheses.js
+++ js/parentheses.js
@@ -1,7 +1,18 @@
 function isBalanced(string) {
   len = string.length
   if (len == 0) return true;
-  string = string.replace('()', '')
+  string = deletePair(string)
   if (string.length == len) return false;
   return isBalanced(string)
+}
+
+function deletePair(string) {
+  return tryPair(string, 0);
+
+  function tryPair(string, index) {
+    if (string.substr(index, 2) == '()')
+      return string.slice(0, index) + string.slice(index + 2);
+    if (index < string.length - 1) return tryPair(string, index + 1);
+    return string
+  }
 }
